5cc75b41a9ed88fa62f8dc33fa5f0fa0af676c82
andreaforesgil <andreafores@gmail.com>
2025-06-10 21:05:05 +0200

    config docker

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: '/api/',
+  stripePublishableKey: 'pk_test_51RFZnuIqweLL5el9IBW0eLcWKrEkCOgga2cOx0oISnZklD9kyg13XlSw0ugXL7mopFB58HsE3CWOScIdblE2G1fZ003Iwd2MYf'
 };
